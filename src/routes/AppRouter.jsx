072b31e2436f550f6d1f473e665155604c19f0ec
Juanjo <juanjo.desarrollador@gmail.com>
2022-05-15 01:50:48 -0400

    básico con rutas protegidas, sin redux

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+import { Home } from "../components/Home";
+import { Us } from "../components/Us";
+import { PrivateRoute } from "./PrivateRoute";
+  export const AppRouter = () => {
+      const isLoggedIn = false;
+      return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route exact path='/' element={<PrivateRoute isLoggedIn={isLoggedIn}/>}>
+                        <Route exact path='/us' element={<Us/>}/>
+                    </Route>
+                </Routes>
+            </Router>
+      );
+    };
+        
